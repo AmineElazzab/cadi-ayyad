7adc0ad602cbfb613e7d2b4e50c9b3457ec793a5
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-06-19 10:33:35 +0100

    create React Typscript app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Bibliotheque from "./pages/Nos_Bibliotheques";
+import Nouveautés from "./pages/Nouveautés";
+import Propose from "./pages/A_Propose";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/bibliotheque" element={<Bibliotheque />} />
+          <Route path="/nouveautes" element={<Nouveautés />} />
+          <Route path="/propose" element={<Propose />} />
+          <Route
+            path="*"
+            element={
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100vh",
+                  backgroundColor: "black",
+                }}
+              >
+                <h1
+                  style={{
+                    fontSize: "10rem",
+                    color: "white",
+                    textShadow: "0 0 1rem white",
+                  }}
+                >
+                  404
+                </h1>
+              </div>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
