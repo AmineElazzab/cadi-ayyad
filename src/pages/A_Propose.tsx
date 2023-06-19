0714303a000d60cf89586b0d4b24004c25894c88
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-06-19 10:37:54 +0100

    A_Propose Page

---
+++ src/pages/A_Propose.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/hero";
+import Prop from "../components/propose";
+
+function Propose() {
+  return (
+    <div className="bg-[#F5F5F5]">
+      <Hero />
+      <Prop />
+    </div>
+  );
+}
+
+export default Propose;
