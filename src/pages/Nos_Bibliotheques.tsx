044986bba855a4025398b03e5b9fb22dc4d1a9b6
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-06-19 10:38:11 +0100

    Nos_Bibliotheques Page

---
+++ src/pages/Nos_Bibliotheques.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/hero";
+import Biblio from "../components/bibliotheque";
+
+function Bibliotheque() {
+  return (
+    <div className="bg-[#F5F5F5] ">
+      <Hero />
+      <Biblio />
+    </div>
+  );
+}
+
+export default Bibliotheque;
