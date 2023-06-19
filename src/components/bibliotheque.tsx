b91ba483002b07ac7ec97016646a39782e444a6b
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-06-19 10:35:04 +0100

    Bibliotheque Copmonent

---
+++ src/components/bibliotheque.tsx
@@ -0,0 +1,288 @@
+import React from "react";
+import icon from "../assets/icon.png";
+import fsjes from "../assets/fsjes.png";
+import groupe from "../assets/group.png";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const Biblio: React.FC = () => {
+  return (
+    <div className="mb-0 max-h-screen flex justify-center">
+      <div className=" grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-flow-col-1 gap-5 mt-10 mb-10">
+        <div className="flex flex-col p-8 rounded-lg bg-white  translate-x-4 translate-y-4 w-[308px] md:w-[308px]">
+          <div className="mt-3 flex justify-center ">
+            <img src={groupe} alt="" />
+            <div className="absolute inset-0 object-cover left-[272px] top-[10px]  ">
+              <img
+                src={icon}
+                alt=""
+                //
+                className="absolute inset-0 object-cover "
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <img src={fsjes} alt="" />
+          </div>
+          <div className="my-4 flex justify-center">
+            <span className="font-normal text-base">
+              Faculté des Sciences Semlalia
+            </span>
+          </div>
+          <div className="flex justify-center items-center font-semibold">
+            <button className="px-4 py-3 w-[135px] border border-[#A5541C]  mt-4">
+              Voir plus
+              <ArrowForwardIcon
+                className="
+                ml-2
+                w-4
+                h-4
+                fontSize small
+                "
+              />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col p-8 rounded-lg bg-white  translate-x-4 translate-y-4 w-[308px] md:w-[308px]">
+          <div className="mt-3 flex justify-center ">
+            <img src={groupe} alt="" />
+            <div className="absolute inset-0 object-cover left-[272px] top-[10px]  ">
+              <img
+                src={icon}
+                alt=""
+                //
+                className="absolute inset-0 object-cover "
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <img src={fsjes} alt="" />
+          </div>
+          <div className="my-4 flex justify-center">
+            <span className="font-normal text-base">
+              Faculté des Sciences Semlalia
+            </span>
+          </div>
+          <div className="flex justify-center items-center font-semibold">
+            <button className="px-4 py-3 w-[135px] border border-[#A5541C]  mt-4">
+              Voir plus
+              <ArrowForwardIcon
+                className="
+                ml-2
+                w-4
+                h-4
+                fontSize small
+                "
+              />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col p-8 rounded-lg bg-white  translate-x-4 translate-y-4 w-[308px] md:w-[308px]">
+          <div className="mt-3 flex justify-center ">
+            <img src={groupe} alt="" />
+            <div className="absolute inset-0 object-cover left-[272px] top-[10px]  ">
+              <img
+                src={icon}
+                alt=""
+                //
+                className="absolute inset-0 object-cover "
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <img src={fsjes} alt="" />
+          </div>
+          <div className="my-4 flex justify-center">
+            <span className="font-normal text-base">
+              Faculté des Sciences Semlalia
+            </span>
+          </div>
+          <div className="flex justify-center items-center font-semibold">
+            <button className="px-4 py-3 w-[135px] border border-[#A5541C]  mt-4">
+              Voir plus
+              <ArrowForwardIcon
+                className="
+                ml-2
+                w-4
+                h-4
+                fontSize small
+                "
+              />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col p-8 rounded-lg bg-white  translate-x-4 translate-y-4 w-[308px] md:w-[308px]">
+          <div className="mt-3 flex justify-center ">
+            <img src={groupe} alt="" />
+            <div className="absolute inset-0 object-cover left-[272px] top-[10px]  ">
+              <img
+                src={icon}
+                alt=""
+                //
+                className="absolute inset-0 object-cover "
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <img src={fsjes} alt="" />
+          </div>
+          <div className="my-4 flex justify-center">
+            <span className="font-normal text-base">
+              Faculté des Sciences Semlalia
+            </span>
+          </div>
+          <div className="flex justify-center items-center font-semibold">
+            <button className="px-4 py-3 w-[135px] border border-[#A5541C]  mt-4">
+              Voir plus
+              <ArrowForwardIcon
+                className="
+                ml-2
+                w-4
+                h-4
+                fontSize small
+                "
+              />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col p-8 rounded-lg bg-white  translate-x-4 translate-y-4 w-[308px] md:w-[308px]">
+          <div className="mt-3 flex justify-center ">
+            <img src={groupe} alt="" />
+            <div className="absolute inset-0 object-cover left-[272px] top-[10px]  ">
+              <img
+                src={icon}
+                alt=""
+                //
+                className="absolute inset-0 object-cover "
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <img src={fsjes} alt="" />
+          </div>
+          <div className="my-4 flex justify-center">
+            <span className="font-normal text-base">
+              Faculté des Sciences Semlalia
+            </span>
+          </div>
+          <div className="flex justify-center items-center font-semibold">
+            <button className="px-4 py-3 w-[135px] border border-[#A5541C]  mt-4">
+              Voir plus
+              <ArrowForwardIcon
+                className="
+                ml-2
+                w-4
+                h-4
+                fontSize small
+                "
+              />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col p-8 rounded-lg bg-white  translate-x-4 translate-y-4 w-[308px] md:w-[308px]">
+          <div className="mt-3 flex justify-center ">
+            <img src={groupe} alt="" />
+            <div className="absolute inset-0 object-cover left-[272px] top-[10px]  ">
+              <img
+                src={icon}
+                alt=""
+                //
+                className="absolute inset-0 object-cover "
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <img src={fsjes} alt="" />
+          </div>
+          <div className="my-4 flex justify-center">
+            <span className="font-normal text-base">
+              Faculté des Sciences Semlalia
+            </span>
+          </div>
+          <div className="flex justify-center items-center font-semibold">
+            <button className="px-4 py-3 w-[135px] border border-[#A5541C]  mt-4">
+              Voir plus
+              <ArrowForwardIcon
+                className="
+                ml-2
+                w-4
+                h-4
+                fontSize small
+                "
+              />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col p-8 rounded-lg bg-white  translate-x-4 translate-y-4 w-[308px] md:w-[308px]">
+          <div className="mt-3 flex justify-center ">
+            <img src={groupe} alt="" />
+            <div className="absolute inset-0 object-cover left-[272px] top-[10px]  ">
+              <img
+                src={icon}
+                alt=""
+                //
+                className="absolute inset-0 object-cover "
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <img src={fsjes} alt="" />
+          </div>
+          <div className="my-4 flex justify-center">
+            <span className="font-normal text-base">
+              Faculté des Sciences Semlalia
+            </span>
+          </div>
+          <div className="flex justify-center items-center font-semibold">
+            <button className="px-4 py-3 w-[135px] border border-[#A5541C]  mt-4">
+              Voir plus
+              <ArrowForwardIcon
+                className="
+                ml-2
+                w-4
+                h-4
+                fontSize small
+                "
+              />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col p-8 rounded-lg bg-white  translate-x-4 translate-y-4 w-[308px] md:w-[308px]">
+          <div className="mt-3 flex justify-center ">
+            <img src={groupe} alt="" />
+            <div className="absolute inset-0 object-cover left-[272px] top-[10px]  ">
+              <img
+                src={icon}
+                alt=""
+                //
+                className="absolute inset-0 object-cover "
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <img src={fsjes} alt="" />
+          </div>
+          <div className="my-4 flex justify-center">
+            <span className="font-normal text-base">
+              Faculté des Sciences Semlalia
+            </span>
+          </div>
+          <div className="flex justify-center items-center font-semibold">
+            <button className="px-4 py-3 w-[135px] border border-[#A5541C]  mt-4">
+              Voir plus
+              <ArrowForwardIcon
+                className="
+                ml-2
+                w-4
+                h-4
+                fontSize small
+                "
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Biblio;
